5889f3e4b8b1ee12ea05c509e48d8410aa72bb3d
9CD1KC2\admin <smithjrs22@gmail.com>
2022-05-10 16:59:04 -0400

    push

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -9,16 +9,29 @@ public class BankAccount {
         this.accountHolderName = accountHolder; 
     }
 
-    public void deposit(double amountToDeposit){
+    // public void deposit(double amountToDeposit){
+    //     bankAccountBalance = amountToDeposit + bankAccountBalance;
+    // }
+    public double deposit(double amountToDeposit){
         bankAccountBalance = amountToDeposit + bankAccountBalance;
+        return amountToDeposit;
+
     }
 
-    public void withdraw(double amountToWithdraw){
+    // public void withdraw(double amountToWithdraw){
+    //     bankAccountBalance = bankAccountBalance - amountToWithdraw;
+    // }
+    public double withdraw(double amountToWithdraw){
         bankAccountBalance = bankAccountBalance - amountToWithdraw;
+        return amountToWithdraw;
     }
 
     public void accountDetails(){
         System.out.println(accountHolderName + " account balance is " + bankAccountBalance);
+    }
+
+    public double transfer(BankAccount account, double amount){
+        return account.deposit(this.withdraw(amount));
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,11 @@ public class Main {
     public static void main(String[] args)  {
         BankAccount newBankAcctOne = new BankAccount(5000, "Larry");
         BankAccount newBankAcctTwo = new BankAccount(300, "Mary");
-        newBankAcctOne.withdraw(100);
-        newBankAcctTwo.deposit(100);  
+        // newBankAcctOne.withdraw(100);
+        // newBankAcctTwo.deposit(100);  
+        newBankAcctOne.transfer(newBankAcctTwo, 100);
         newBankAcctOne.accountDetails();
         newBankAcctTwo.accountDetails();
+        
     }
 }
